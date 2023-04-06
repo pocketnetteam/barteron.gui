4f5916e836c49dba43f58d4550857d8db8a57dfe
Michael L <levinez.michael@gmail.com>
2023-04-06 17:49:22 +0500

    Fix carousel

--- src/components/barter/list/index.js
+++ src/components/barter/list/index.js
@@ -17,7 +17,8 @@ export default {
 
 	data() {
 		return {
-			slide: 0,
+			index: 0,
+			indexPos: 0,
 			prevDisabled: true,
 			nextDisabled: false
 		}
@@ -25,82 +26,77 @@ export default {
 
 	computed: {
 		/**
-		 * Calc item width + grid gap
-		 * 
-		 * @return {Number}
-		 */
-		itemDimensions() {
-			const
-				slide = this.$refs.track.children[0],
-				gap = parseFloat(getComputedStyle(this.$refs.track).gap);
-
-			return {
-				width: slide.offsetWidth,
-				gap: gap,
-				all: slide.offsetWidth + gap
-			};
-		},
-
-		/**
-		 * Calc items count in visible area
+		 * Get grid gap in px
 		 * 
 		 * @returns {Number}
 		 */
-		itemsVisible() {
-			return Array.from(this.$refs.track.children).filter(item => {
-				const
-					holder = this.$refs.holder.getBoundingClientRect(),
-					cell = item.getBoundingClientRect(),
-					dim = this.itemDimensions.all;
+		gridGap() {
+			return parseFloat(getComputedStyle(this.$refs.track).gap);
+		},
 
-				return cell.left >= (holder.left - dim) && cell.right <= (holder.right + dim);
-			}).length;
+		/**
+		 * Get slides
+		 * 
+		 * @returns {Array}
+		 */
+		slides() {
+			return Array.from(this.$refs.track.children);
 		}
 	},
 
 	methods: {
-		prepare() {
-			this.prevDisabled = !(this.slide > 0);
-			this.nextDisabled = !(this.slide < this.items.length - this.itemsVisible);
+		/**
+		 * Animate carousel
+		 * 
+		 * @param {Boolean} next - Direction
+		 */
+		animate(next) {
+			const
+				/* Get count of visible slides */
+				visible = parseInt(this.$refs.holder.offsetWidth / this.slides[0].offsetWidth),
 
-			return {
-				prevDisabled: this.prevDisabled,
-				nextDisabled: this.nextDisabled
-			}
-		},
+				/* Calc item offset at right */
+				offset = (() => {
+					const slide = this.slides[0 + visible];
+					return (slide.offsetLeft + slide.offsetWidth) - this.$refs.holder.offsetWidth;
+				})(),
 
-		animate() {
-			let offset;
+				/* Next track offset by index */
+				nextPos = this.index * (this.slides[0].offsetWidth + this.gridGap);
 
-			if (!this.prepare().nextDisabled) {
-				/* Shift to one item */
-				offset = this.itemDimensions.all * this.slide;
+			/* Enable/Disable controls */
+			this.prevDisabled = this.index < 1;
+			this.nextDisabled = this.index > this.items.length - visible;
+
+			if (!next) {
+				/* Logic for prev action */
+				this.indexPos = nextPos;
 			} else {
-				/* Shift to end of slider */
-				const
-					holder = this.$refs.holder.getBoundingClientRect(),
-					last = this.$refs.track.children[this.items.length - 1].getBoundingClientRect(),
-					dim = this.itemDimensions,
-					pos = last.right - dim.width + dim.gap;
-				
-				offset = pos > holder.right ? pos : this.itemDimensions.all * this.slide
+				/* Logic for next action */
+				const maxPos = (this.items.length - visible - 1) * (this.slides[0].offsetWidth + this.gridGap) + offset;
+
+				this.indexPos = nextPos <= maxPos ? nextPos : maxPos;
+				if (nextPos >= maxPos) this.index = this.items.length - visible;
 			}
 
-			this.$refs.track.style.transform = `translateX(-${ offset }px)`;
+			/* Shift track */
+			this.$refs.track.style.transform = `translateX(-${ this.indexPos }px)`;
 		},
 
+		/**
+		 * Prev button handler
+		 */
 		prev() {
-			if (!this.prepare().prevDisabled) {
-				this.slide--;
-				this.animate();
-			}
+			if (this.index > 0) this.index--;
+			this.animate(false);
 		},
 
+		/**
+		 * Next button handler
+		 */
 		next() {
-			if (!this.prepare().nextDisabled) {
-				this.slide++;
-				this.animate();
-			}
+			if (this.index < this.items.length - 1) this.index++;
+			this.animate(true);
 		}
 	}
 }
