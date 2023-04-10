f2e0662d7b48a5e1b215153ce806f389e96f7886
Michael L <levinez.michael@gmail.com>
2023-04-10 22:09:34 +0500

    add new packages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+	transpileDependencies: true,
+	pluginOptions: {
+		i18n: {
+			locale: "en-US",
+			fallbackLocale: "en-US",
+			localeDir: "i18n",
+			enableInSFC: false
+		}
+	}
 })
