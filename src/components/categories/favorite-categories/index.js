0137ba10b8abce3e43d25ec994c6e87fda09e38f
Michael L <levinez.michael@gmail.com>
2023-04-11 21:08:29 +0500

    Change sub-categories to favorite

---
+++ src/components/categories/favorite-categories/index.js
@@ -0,0 +1,22 @@
+export default {
+	name: "FavoriteCategories",
+
+	props: {
+		items: {
+			type: Array,
+			default: () => []
+		}
+	},
+	
+	methods: {
+		/**
+		 * Get localization text
+		 * 
+		 * @param {String} text 
+		 * @return {String}
+		 */
+		text(text) {
+			return this.$te(text) ? this.$t(text) : text;
+		}
+	}
+}
---
+++ src/components/categories/favorite-categories/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<nav class="favorite-categories">
+		<ul>
+			<li
+				v-for="(item, index) in items"
+				:key="index"
+			>
+				<router-link :to="{ name: 'category', params: { slug: item.name } }">
+					{{ text(item.name) }}
+				</router-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<style scoped lang="sass" src="./index.sass"></style>
+<script src="./index.js"></script>
