036a1e2ae9c982830aa516ab770412f80e9276cf
Michael L <levinez.michael@gmail.com>
2023-04-11 20:15:26 +0500

    Fix categories and breadcrumbs

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,8 @@
 <style lang="sass" src="@/css/main.sass"></style>
 <style src="@/assets/font-awesome/css/all.css"></style>
 <script>
-import categories from "@/data/categories.json";
+import Categories from "@/js/categories.js"
+import categoriesData from "@/data/categories.json";
 import Header from "@/components/layout/header/index.vue";
 import Footer from "@/components/layout/footer/index.vue";
 
@@ -44,71 +45,8 @@ export default {
 
 	computed: {
 		categories() {
-			const $ = this;
-			/**
-			 * @class Categories
-			 */
-			class Categories {
-				/**
-				 * @param {Object} data
-				 */
-				constructor(data) {
-					this.items = data;
-					return this;
-				}
-
-				/**
-				 * Get category as Object
-				 * 
-				 * @param {Number} id
-				 * @return {Object}
-				 */
-				get(id) {
-					return Object.assign({ id: id }, this.items[id]);
-				}
-
-				/**
-				 * Search through items
-				 * 
-				 * @param {String} param
-				 * @param {Number, String} value
-				 * @return {Object}
-				 */
-				find(param, value) {
-					for (let id in this.items) {
-						if (items[id][param] === value) {
-							return this.get(id);
-						}
-					}
-				}
-
-				/**
-				 * Search by id
-				 * 
-				 * @param {Number, Array[Number]} id
-				 * @return {Object}
-				 */
-				findById(id) {
-					return Array.isArray(id) ? id.map(n => this.get(n)) : this.get(id);
-				}
-
-				/**
-				 * Search by name
-				 * 
-				 * @param {String, Array[String]} name
-				 * @return {Object}
-				 */
-				findByName(name) {
-					return Array.isArray(name) ? name.map(n => this.find("name", n)) : this.find("name", name);
-				}
-			};
-
-			return new Categories(categories);
+			return new Categories(categoriesData);
 		}
-	},
-
-	mounted() {
-		// console.log(this.categories.findByName("office_furniture"))
 	}
 }
 </script>
--- src/components/breadcrumbs/index.js
+++ src/components/breadcrumbs/index.js
@@ -1,31 +1,98 @@
 export default {
 	name: "Breadcrumbs",
 
-	data() {
-		return {
-			matches: (() => {
-				let matches = this.$route.matched;
+	inject: ["categories"],
 
-				matches = matches.map(match => {
-					switch(match.name) {
-						case "category": {
-							match = Object.assign(
-								Object.assign({}, this.$route),
-								{ name: this.$route.params.slug }
-							);
-							break;
-						}
+	props: {
+		parent: [String, Array]
+	},
+
+	computed: {
+		matches() {
+			const match = [];
+
+			switch(this.$route.matched[0].name) {
+				case "category": {
+					/* Add category parents to breadcrumbs */
+					let
+						page = this.categories.findByName(this.$route.params.slug),
+						cat = page;
+					
+					/* Add parent to breadcrumbs */
+					while (cat.parent) {
+						cat = this.sub(cat.id ? cat : this.$route.params.slug);
+						match.push(
+							this.path(cat, "/category")
+						);
 					}
 
-					return match;
-				});
+					/* Add category to breadcrumbs */
+					match.reverse().push(
+						this.path(page, "/category")
+					);
 
-				if (matches.length < 2) {
-					matches = [this.$router.options.routes[0]].concat(matches);
+					page = cat = null;
+
+					break;
 				}
+			}
+			
+			if (this.parent) {
+				return this.getParents().concat(match);
+			}
 
-				return matches;
-			})()
+			return match;
+		}
+	},
+
+	methods: {
+		/**
+		 * Get parents by name
+		 * 
+		 * @return {Array}
+		 */
+		getParents() {
+			return this.$router.options.routes.filter(f => {
+				if (Array.isArray(this.parent)) {
+					return this.parent.find(name => name === f.name);
+				} else {
+					return f.name === this.parent;
+				}
+			});
+		},
+
+		/**
+		 * Get sub-categories
+		 * 
+		 * @param {String} name
+		 * @return {Object}
+		 */
+		sub(name) {
+			const category = name.id ? name : this.categories.findByName(name);
+
+			return this.categories.findById(category.parent);
+		},
+
+		/**
+		 * Add path to page
+		 * 
+		 * @param {*} page
+		 * @return {Object}
+		 */
+		path(page, path) {
+			return Object.assign({
+				to: `${ path }/${ page.name }`
+			}, page);
+		},
+	
+		/**
+		 * Get localization text
+		 * 
+		 * @param {String} text 
+		 * @return {String}
+		 */
+		text(text) {
+			return this.$t(`${ this.$te(text) ? '' : 'pageLabels.' }${ text }`);
 		}
 	}
 }
