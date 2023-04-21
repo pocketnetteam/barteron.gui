c4b5b9775c4bb10b2c81b50821a1ffa79197b02f
Michael L <levinez.michael@gmail.com>
2023-04-21 16:37:24 +0500

    Add v-input

--- src/components/v-form/v-input/index.js
+++ src/components/v-form/v-input/index.js
@@ -2,10 +2,19 @@ export default {
 	name: "Vinput",
 
 	props: {
-		id: [String, Array],
-		name: [String, Array],
-		type: [String, Array],
-		value: [String, Array]
+		id: { type: [String, Array], default: () => [] },
+		name: { type: [String, Array], default: () => [] },
+		type: { type: [String, Array], default: () => [] },
+		min: { type: [Number, String, Array], default: () => [] },
+		max: { type: [Number, String, Array], default: () => [] },
+		placeholder: { type: [String, Array], default: () => [] },
+		value: { type: [String, Array], default: () => [] }
+	},
+
+	data() {
+		return {
+			inputs: this.getInputs([this.id, this.name, this.type, this.placeholder, this.value])
+		}
 	},
 
 	methods: {
@@ -15,7 +24,56 @@ export default {
 		 * @param {String} param
 		 */
 		toArray(param) {
-			return (typeof param === "string" ? [param] : Array.isArray(param) ? param : []);
+			return Array.isArray(param) ? param : [param];
+		},
+
+		/**
+		 * Build input list from props
+		 * 
+		 * @param {Array} inputs 
+		 * @return {Object[]}
+		 */
+		getInputs(inputs) {
+			const input = Object.keys(this.$props).reduce((o, p) => {
+				o[p] = this.toArray(this[p]);
+
+				return o;
+			}, {});
+
+			return inputs
+				.map(m => this.toArray(m))
+				.sort((a, b) => a.length > b.length ? -1 : (a.length < b.length ? 1 : 0))[0]
+				.reduce((a, v, i) => {
+					a.push(
+						/* Generate input keys */
+						Object.keys(input).reduce((o, k) => {
+							if (k === "type") o[k] = this.getType(input[k][i] ?? input[k][input[k].length - 1]);
+							else if (["min", "max"].includes(k)) o[k] = input[k][i] ?? input[k][input[k].length - 1];
+							else o[k] = input.placeholder[i] ?? null
+	
+							return o;
+						}, {})
+					);
+
+					return a;
+				}, []);
+		},
+
+		/**
+		 * Get input type
+		 * 
+		 * @param {String}
+		 * @return {String}
+		 */
+		getType(type) {
+			switch(type) {
+				case "minmax": return "number";
+				default: return type;
+			}
 		}
-	}
+	},
+
+	mounted() {
+		console.log(this.inputs)
+	},
 }
--- src/components/v-form/v-input/index.vue
+++ src/components/v-form/v-input/index.vue
@@ -2,8 +2,20 @@
 	<div class="v-input-holder">
 		<slot name="before"></slot>
 
-		<div class="v-input">
-
+		<div
+			v-for="(input, index) in inputs"
+			:key="index"
+			class="v-input"
+		>
+			<input
+				:id="input.id"
+				:name="input.name"
+				:type="input.type"
+				:min="input.min"
+				:max="input.max"
+				:placeholder="input.placeholder"
+				:value="input.value"
+			/>
 		</div>
 
 		<slot name="after"></slot>
