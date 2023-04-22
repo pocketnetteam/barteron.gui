45e6570ed0cadfd53dfa8609e3e3c704812633a8
Michael L <levinez.michael@gmail.com>
2023-04-22 12:13:11 +0500

    add v-switch

---
+++ src/components/v-form/v-switch/index.js
@@ -0,0 +1,20 @@
+export default {
+	name: "Vswitch",
+
+	props: {
+		id: {
+			type: String,
+			required: true
+		},
+		name: String,
+		type: String,
+		value: String,
+		label: String,
+		
+		vSize: String,
+	},
+
+	methods: {
+		
+	}
+}
---
+++ src/components/v-form/v-switch/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="v-switch-holder">
+		<slot name="before"></slot>
+
+		<div
+			:class="{
+				'v-switch': true,
+				[`v-switch-${ vSize ?? 'md' }`]: true
+			}"
+		>
+			<input
+				:id="id"
+				:name="name"
+				:type="type"
+				:value="value"
+			/>
+			<span class="v-control"></span>
+			<label v-if="label" :for="id">{{ label }}</label>
+		</div>
+
+		<slot name="after"></slot>
+	</div>
+</template>
+
+<style lang="sass" src="./index.sass"></style>
+<script src="./index.js"></script>
