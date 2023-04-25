7d91825c4f7e9edd65d96289bd9b60a05fe0e005
Michael L <levinez.michael@gmail.com>
2023-04-25 09:40:15 +0500

    add select and input with labels

--- src/views/Barter/Create/content/index.vue
+++ src/views/Barter/Create/content/index.vue
@@ -1,12 +1,20 @@
 <template>
-	<v-content>
+	<v-content class="shrink">
 		<div class="row">
 			<strong class="title">{{ $t('steps.propose') }}</strong>
 		</div>
 
-		<div class="row">
-			<v-input
-				id="title"
+		<div class="row block">
+			<label for="title" class="v-label">{{ $t('title') }}</label>
+			<v-input id="title" />
+		</div>
+		
+		<div class="row block">
+			<label for="category" class="v-label">{{ $t('category.label') }}</label>
+			<v-select
+				id="category"
+				class="full-width"
+				:dropdown="[{ text: 'Category', value: 'category' }]"
 			/>
 		</div>
 	</v-content>
