94d9b2cc64b3871a688497f32085d4c526833d70
Michael L <levinez.michael@gmail.com>
2023-04-25 17:42:18 +0500

    add new elements

--- src/views/Barter/Create/content/index.vue
+++ src/views/Barter/Create/content/index.vue
@@ -1,21 +1,99 @@
 <template>
 	<v-content class="shrink">
-		<div class="row">
-			<strong class="title">{{ $t('steps.propose') }}</strong>
-		</div>
+		<strong class="title">{{ $t('steps.propose') }}</strong>
 
 		<div class="row block">
 			<label for="title" class="v-label">{{ $t('title') }}</label>
-			<v-input id="title" />
+			<v-input id="title" vSize="lg" />
 		</div>
 		
-		<div class="row block">
+		<div class="row block sep">
 			<label for="category" class="v-label">{{ $t('category.label') }}</label>
 			<v-select
 				id="category"
 				class="full-width"
 				:dropdown="[{ text: 'Category', value: 'category' }]"
+				vSize="lg"
 			/>
+		</div>
+
+		<div class="row block sep">
+			<strong class="title">{{ $t('steps.photos') }}</strong>
+			<button class="photo-uploader">
+				<input type="file">
+				<span class="text">
+					<i class="fa fa-plus"></i>
+					<span>{{ $t('upload_image') }}</span>
+				</span>
+			</button>
+			<p>{{ $t('upload_image_text') }}</p>
+		</div>
+
+		<div class="row block">
+			<strong class="title">{{ $t('steps.get') }}</strong>
+			<ul class="radio-list">
+				<li>
+					<v-switch
+						type="radio"
+						name="want_get"
+						:label="$t('my_list')"
+						vType="button"
+						vSize="lg"
+					/>
+				</li>
+
+				<li>
+					<v-switch
+						type="radio"
+						name="want_get"
+						checked="checked"
+						:label="$t('something')"
+						vType="button"
+						vSize="lg"
+					/>
+				</li>
+
+				<li>
+					<v-switch
+						type="radio"
+						name="want_get"
+						:label="$t('for_nothing')"
+						vType="button"
+						vSize="lg"
+					/>
+				</li>
+			</ul>
+		</div>
+
+		<div class="row block">
+			<v-select
+				id="category"
+				class="full-width"
+				:dropdown="[{ text: $t('choose_category'), value: '' }, { text: 1, value: 1 }]"
+				vSize="lg"
+			/>
+		</div>
+
+		<div class="row block">
+			<v-input id="exchange_to" :placeholder="$t('steps.get')" vSize="lg" />
+		</div>
+
+		<div class="row block">
+			<label for="currency" class="v-label">{{ $t('choose_currency_text') }}</label>
+			<v-input
+				:id="['currency', 'pkoin']"
+				:type="['number', 'number']"
+				:value="['0', '0']"
+				vSize="lg"
+			>
+				<template v-slot:input0After>
+					<v-select :dropdown="[{ text: 'USD', value: 'usd' }]" />
+				</template>
+
+				<template v-slot:input1After>
+					<span class="icon">PKOIN</span>
+				</template>
+			</v-input>
 		</div>
 	</v-content>
 </template>
