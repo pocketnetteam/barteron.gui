e89048ee15594b2d53cd5d81ff24992413c00de7
Michael L <levinez.michael@gmail.com>
2023-04-26 16:12:33 +0500

    add v-map component

---
+++ src/components/v-form/v-map/index.js
@@ -0,0 +1,13 @@
+export default {
+	name: "Vmap",
+
+	props: {
+		id: String,
+		height: String,
+		width: String
+	},
+
+	mounted() {
+		
+	}
+}
---
+++ src/components/v-form/v-map/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="map-holder">
+		<div :id="id"></div>
+	</div>
+</template>
+
+<style lang="sass" src="./index.sass"></style>
+<script src="https://maps.api.2gis.ru/2.0/loader.js?pkg=full"></script>
+<script src="./index.js"></script>
