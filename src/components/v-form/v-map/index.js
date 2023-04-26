7a486a39724f7ef403f77af85e6bb76360972376
Michael L <levinez.michael@gmail.com>
2023-04-26 20:06:17 +0500

    modify v-map

--- src/components/v-form/v-map/index.js
+++ src/components/v-form/v-map/index.js
@@ -2,12 +2,45 @@ export default {
 	name: "Vmap",
 
 	props: {
-		id: String,
-		height: String,
-		width: String
+		id: {
+			type: String,
+			default: `map-${ (+new Date).toString(16) }`
+		},
+		height: {
+			type: String,
+			default: "260px"
+		},
+		width: {
+			type: String,
+			default: "100%"
+		},
+	},
+
+	data() {
+		return {
+			script: null,
+			map: null
+		}
+	},
+
+	created() {
+		this.script = document.createElement("script");
+		this.script.type = "text/javascript";
+		this.script.src = "https://maps.api.2gis.ru/2.0/loader.js?pkg=full";
+		document.head.append(this.script);
 	},
 
 	mounted() {
-		
+		this.$refs.map.style.setProperty('--height', this.height);
+		this.$refs.map.style.setProperty('--width', this.width);
+
+		this.script.onload = () => {
+			DG.then(() => {
+					this.map = DG.map(this.id, {
+							center: [54.98, 82.89],
+							zoom: 13
+					});
+			});
+		}
 	}
 }
--- src/components/v-form/v-map/index.vue
+++ src/components/v-form/v-map/index.vue
@@ -1,9 +1,6 @@
 <template>
-	<div class="map-holder">
-		<div :id="id"></div>
-	</div>
+	<div :id="id" ref="map"></div>
 </template>
 
-<style lang="sass" src="./index.sass"></style>
-<script src="https://maps.api.2gis.ru/2.0/loader.js?pkg=full"></script>
+<style scoped lang="sass" src="./index.sass"></style>
 <script src="./index.js"></script>
