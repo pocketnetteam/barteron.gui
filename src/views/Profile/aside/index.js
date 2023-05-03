9500d5c2d29378626d1e45ac02f21aac51e524dd
Michael L <levinez.michael@gmail.com>
2023-05-03 20:01:48 +0500

    changes in profile template

--- src/views/Profile/aside/index.js
+++ src/views/Profile/aside/index.js
@@ -1,19 +1,3 @@
-import SubCategories from "@/components/categories/sub-categories/index.vue";
-
 export default {
-	name: "Aside",
-
-	components: {
-		SubCategories
-	},
-
-	computed: {
-		category() {
-			return this.categories.findByName(this.$route.params.slug);
-		},
-
-		subCategories() {
-			return this.categories.findById(this.category?.children);
-		}
-	}
+	name: "Aside"
 }
--- src/views/Profile/content/index.vue
+++ src/views/Profile/content/index.vue
@@ -1,65 +1,6 @@
 <template>
 	<v-content>
-		<div class="row">
-			<div class="col">
-				<v-select @selected="selectFilter">
-					<template>
-						<span class="value"></span>
-					</template>
-	
-					<template v-slot:dropdown>
-						<option
-							v-for="(filter, index) in filters"
-							:key="index"
-							:value="filter.value"
-						>
-							<i
-								:class="{
-									'fa': true,
-									'fa-sort-amount-up': filter.value === 'new',
-									'fa-sort-amount-up-alt': filter.value === 'old',
-									'fa-sort-numeric-down': filter.value === 'price_asc',
-									'fa-sort-numeric-down-alt': filter.value === 'price_desc',
-									'icon': true
-								}"
-							></i>
-							{{ $t(`filterLabels.${ filter.value }`) }}
-						</option>
-					</template>
-				</v-select>
-			</div>
-
-			<div class="col right">
-				<v-select @selected="selectView">
-					<template>
-						<span class="value"></span>
-					</template>
-	
-					<template v-slot:dropdown>
-						<option
-							v-for="(view, index) in views"
-							:key="index"
-							:value="view.value"
-						>
-							<i
-								:class="{
-									'fa': true,
-									'fa-th-large': view.value === 'tail',
-									'fa-align-justify': view.value === 'row',
-									'icon': true
-								}"
-							></i>
-							{{ $t(`viewLabels.${ view.value }`) }}
-						</option>
-					</template>
-				</v-select>
-			</div>
-		</div>
-
-		<BarterList
-			:items="barters.slice(0, 8)"
-			:vType="bartersView"
-		/>
+		
 	</v-content>
 </template>
 
