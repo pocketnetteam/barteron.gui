a0d17459f211dfd68866a4de0ee343ba509ab7ed
Michael L <levinez.michael@gmail.com>
2023-06-22 10:11:59 +0500

    push part to update state on another device

--- src/views/Profile/aside/index.vue
+++ src/views/Profile/aside/index.vue
@@ -1,15 +1,46 @@
 <template>
 	<v-aside>
+		<!-- Profile stats -->
 		<div class="box profile">
-			<div class="top">
-				<picture class="status-online">
-					<img src="@/assets/images/avatar.png" alt="avatar">
-				</picture>
-			</div>
-			<div class="edit">
+			<div class="row author">
+				<div class="col">
+					<picture class="status-online">
+						<img src="@/assets/images/avatar.png" alt="avatar">
+					</picture>
+				</div>
 
+				<div class="col user">
+					<ul class="list">
+						<li>
+							<strong class="label">Username</strong>
+						</li>
+
+						<li>
+							<ul class="stars">
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+							</ul>
+						</li>
+
+						<li>
+							<span>0.0 {{ $t('rating.label').toLowerCase() }} (0 {{ $t('rating.reviews').toLowerCase() }})</span>
+						</li>
+					</ul>
+				</div>
 			</div>
-			<div class="info">
+
+			<!-- Profile edit -->
+			<div class="row edit">
+				<v-button vType="stroke-bulma">
+					{{ $t('profile.edit') }}
+				</v-button>
+			</div>
+
+			<!-- Profile info -->
+			<div class="row info">
 				
 			</div>
 		</div>
