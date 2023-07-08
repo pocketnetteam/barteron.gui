b8c578493ad32e348ab40680643973fd4320754a
Michael L <levinez.michael@gmail.com>
2023-07-08 12:33:48 +0500

    add content for profile

--- src/views/Profile/content/index.js
+++ src/views/Profile/content/index.js
@@ -1,13 +1,49 @@
+import BarterList from "@/components/barter/list/index.vue";
+
 export default {
 	name: "Content",
 
+	components: {
+		BarterList
+	},
+
 	data() {
 		return {
-			
+			bartersView: "tile"
+		}
+	},
+
+	computed: {
+		/**
+		 * Make list of view
+		 * 
+		 * @return {Array}
+		 */
+		views() {
+			return this.parseLabels("viewLabels");
 		}
 	},
 
 	methods: {
-		
+		/**
+		 * Parse labels object from localization
+		 * 
+		 * @param {String} label 
+		 * @return {Array}
+		 */
+		parseLabels(label) {
+			return Object.keys(this.$t(label)).map((value, index) => {
+				return { text: this.$t(`${ label }.${ value }`), value, default: index === 0 };
+			});
+		},
+
+		/**
+		 * View change callback
+		 * 
+		 * @param {Object} view 
+		 */
+		selectView(view) {
+			this.bartersView = view?.value;
+		}
 	}
 }
--- src/views/Profile/content/index.vue
+++ src/views/Profile/content/index.vue
@@ -1,6 +1,163 @@
 <template>
 	<v-content>
-		
+		<!-- Primary tabs level -->
+		<v-tabs
+			:tabset="[
+				{
+					tabId: 'my-ads',
+					title: `<i class='fa fa-list'></i> ${ $t('profile.my_ads') } (0)`
+				},
+				{
+					tabId: 'my-barters',
+					title: `<i class='fa fa-sync-alt'></i> ${ $t('profile.my_barters') } (0)`
+				},
+				{
+					tabId: 'messages',
+					title: `<i class='fa fa-comment'></i> ${ $t('profile.messages') } (0)`
+				},
+				{
+					tabId: 'favorites',
+					title: `<i class='fa fa-heart'></i> ${ $t('profile.favorites') }`
+				},
+				{
+					tabId: 'feedbacks',
+					title: `<i class='fa fa-comments'></i> ${ $t('profile.feedbacks') }`
+				}
+			]"
+			vType="pills"
+		>
+			<!-- Tab: My ads -->
+			<template #my-ads>
+				<!-- Secondary tabs level -->
+				<v-tabs
+					class="inner-tabs"
+					:tabset="[
+						{
+							tabId: 'active',
+							title: $t('profile.active')
+						},
+						{
+							tabId: 'inactive',
+							title: $t('profile.inactive')
+						}
+					]"
+				>
+					<!-- After tabset -->
+					<template #after>
+						<v-select @selected="selectView">
+							<template>
+								<span class="value"></span>
+							</template>
+			
+							<template #dropdown>
+								<option
+									v-for="(view, index) in views"
+									:key="index"
+									:value="view.value"
+								>
+									<i
+										:class="{
+											'fa': true,
+											'fa-th-large': view.value === 'tile',
+											'fa-align-justify': view.value === 'row',
+											'icon': true
+										}"
+									></i>
+									{{ $t(`viewLabels.${ view.value }`) }}
+								</option>
+							</template>
+						</v-select>
+					</template>
+
+					<!-- Tab: Active -->
+					<template #active>
+						<BarterList
+							:items="barters.generate(8)"
+							:vType="bartersView"
+						>
+							<!-- 3-dots dropdown -->
+							<template #favorite>
+								<v-button
+									vType="transparent"
+									vSize="xs"
+								>
+									<i class="fa fa-ellipsis-v"></i>
+									<template #dropdown>
+										<div>
+											123
+										</div>
+									</template>
+								</v-button>
+							</template>
+
+							<!-- Date range, views and favorites -->
+							<template #info="{ item }">
+								<v-switch
+									class="no-padding"
+									type="checkbox"
+									:label="$t('item.autorenew')"
+								/>
+
+								<ul>
+									<li v-if="item?.published">
+										<dl>
+											<dt><i class="fa fa-calendar"></i></dt>
+											<dd>
+												<time>
+													{{ $d(item.published, 'short') }} -
+													{{ $d(item.until, 'short') }}
+												</time>
+											</dd>
+										</dl>
+									</li>
+								</ul>
+
+								<dl>
+									<dt><i class="fa fa-eye"></i></dt>
+									<dd>33</dd>
+									<dt><i class="fa fa-heart"></i></dt>
+									<dd>99</dd>
+								</dl>
+							</template>
+
+							<!-- Edit and find exchange -->
+							<template #offer="{ item }">
+								<v-button :to="`/barter/edit/${ item.id }`">{{ $t('item.edit') }}</v-button>
+								<v-button vType="stroke-hit">{{ $t('item.find_exchange') }}</v-button>
+							</template>
+						</BarterList>
+					</template>
+
+					<!-- Tab: Inactive -->
+					<template #inactive>
+						<BarterList
+							:items="barters.generate(8)"
+							:vType="bartersView"
+						/>
+					</template>
+				</v-tabs>
+			</template>
+
+			<!-- Tab: My barters -->
+			<template #my-barters>
+				My barters
+			</template>
+
+			<!-- Tab: Messages -->
+			<template #messages>
+				Messages
+			</template>
+
+			<!-- Tab: Favorites -->
+			<template #favorites>
+				Favorites
+			</template>
+
+			<!-- Tab: Feedbacks -->
+			<template #feedbacks>
+				Feedbacks
+			</template>
+		</v-tabs>
 	</v-content>
 </template>
 
