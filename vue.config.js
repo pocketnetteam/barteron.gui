00bc9e400c3a9f45553d7034a9b8b33c822bdfca
Michael L <levinez.michael@gmail.com>
2023-07-08 12:24:54 +0500

    change server to https

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 	transpileDependencies: true,
+	devServer: {
+		open: process.platform === "darwin",
+		host: "0.0.0.0",
+		port: 8080, // CHANGE YOUR PORT HERE!
+		https: true,
+		client: {
+			webSocketURL: 'ws://0.0.0.0:8080/ws',
+		},
+		headers: { "Access-Control-Allow-Origin": "*" }
+	},
 	pluginOptions: {
 		i18n: {
 			locale: "en-US",
