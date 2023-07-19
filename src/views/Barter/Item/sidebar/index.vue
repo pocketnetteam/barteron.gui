05b1ca7d0e9bb295708f9f9ff43e9db763665b67
Michael L <levinez.michael@gmail.com>
2023-07-20 02:11:39 +0500

    update barter inner aside

--- src/views/Barter/Item/sidebar/index.vue
+++ src/views/Barter/Item/sidebar/index.vue
@@ -1,6 +1,29 @@
 <template>
 	<v-sidebar>
-		123
+		<div class="box">
+			<h1 class="title">{{ item.name }}</h1>
+			<span class="price">
+				<span class="currency pkoin"></span> {{ item.price }}
+			</span>
+
+			<div class="row t-sep">
+				<BarterExchange :item="item" :items="barters.generate(8)" />
+			</div>
+		</div>
+
+		<div class="box">
+			<Profile />
+			<ExchangeList>
+				<template #edit>
+					<dl class="list">
+						<dt>#</dt>
+						<dd>{{ item.id }}</dd>
+					</dl>
+
+					<a class="report" href="#"><i class="fa fa-flag"></i> {{ $t('profile.report_abuse') }}</a>
+				</template>
+			</ExchangeList>
+		</div>
 	</v-sidebar>
 </template>
 
