817364026fc891b427b34c5c419f98a1c282f8c7
Michael L <levinez.michael@gmail.com>
2023-08-22 23:12:54 +0500

    Remove unnecessary code

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -20,15 +20,6 @@ export default {
 		},
 
 		/**
-		 * Get barteron account
-		 * 
-		 * @return {Object}
-		 */
-		account() {
-			return this.sdk.barteron.account[this.sdk.address];
-		},
-
-		/**
 		 * Get first name from account name
 		 * 
 		 * @return {String}
@@ -36,13 +27,5 @@ export default {
 		shortName() {
 			return this.user?.name.substring(0, 1).toUpperCase();
 		}
-	},
-
-	created() {
-		/* this.sdk.setBrtAccount({
-			address: this.sdk.address,
-			tags: ['x', 'u', 'j']
-		}).then(e => console.log(e, this)); */
-		this.sdk.getBrtAccount().then(e => console.log(e, this));
 	}
 }
