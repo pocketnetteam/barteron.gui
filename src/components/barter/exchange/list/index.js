aa67979c7273674c9551c8781a4ab250fc65d09a
Michael L <levinez.michael@gmail.com>
2023-08-22 23:12:14 +0500

    Modify list component

--- src/components/barter/exchange/list/index.js
+++ src/components/barter/exchange/list/index.js
@@ -5,23 +5,27 @@ export default {
 		tags: {
 			type: Array,
 			default: () => [
-				"Electronics",
-				"Dress",
-				"Shoes",
-				"Beauty & Health",
-				"Sport & Recreation",
-				"Computers",
-				"Food",
-				"Services",
-				"Auto",
-				"Party",
+				"electronics",
+				"belts",
+				"shoes",
+				"beauty_personal_care",
+				"sports"
 			]
+		},
+		visible: {
+			type: Number,
+			default: 5
 		}
 	},
 
 	data() {
 		return {
-			show: 5
+			instance: this, /* Give instance to slot */
+			editable: false,
+			vTags: [].concat(this.tags),
+			show: this.visible,
+			btnAddDisabled: true,
+			list: []
 		}
 	},
 
@@ -30,11 +34,121 @@ export default {
 		 * Toggle items to see
 		 */
 		toggle() {
-			if (this.show < this.tags.length) {
-				this.show = this.tags.length;
+			if (this.show < this.vTags.length) {
+				this.show = this.vTags.length;
 			} else {
-				this.show = 5;
+				this.show = this.visible;
 			}
+		},
+
+		/**
+		 * Validate input value
+		 * 
+		 * @param {Boolean} check
+		 * 
+		 * @return {Boolean|Void}
+		 */
+		validate(check) {
+			const
+				input = this.$refs.tag,
+				list = this.$refs.list,
+				selected = document.evaluate(
+					`//option[text()="${ input.value }"]`,
+					list,
+					null,
+					XPathResult.ANY_TYPE,
+					null
+				)?.iterateNext();
+
+			if (check === true) {
+				/* Check is value in list range */
+				return !!selected;
+			} else {
+				/* Fill input value from option */
+				this.btnAddDisabled = true;
+				input.dataset.value = "";
+
+				if (selected) {
+					this.btnAddDisabled = false;
+					input.dataset.value = selected.dataset.value;
+				}
+			}
+		},
+
+		/**
+		 * Add tag
+		 */
+		add() {
+			const input = this.$refs.tag;
+			
+			if (this.validate(true) && !this.vTags.includes(input.dataset.value)) {
+				this.vTags.push(input.dataset.value);
+				input.value = input.dataset.value = "";
+				this.btnAddDisabled = true;
+			}
+		},
+
+		/**
+		 * Remove tag
+		 * 
+		 * @param {Number} index
+		 */
+		remove(index) {
+			this.vTags.splice(index, 1);
+		},
+
+		/**
+		 * Edit state
+		 */
+		edit() {
+			this.$nextTick(() => {
+				setTimeout(() => this.$refs.tag.focus(), 1);
+			});
+			this.editable = true;
+		},
+
+		/**
+		 * Cancel state
+		 */
+		cancel() {
+			this.vTags = [].concat(this.tags);
+			this.editable = false;
+		},
+
+		/**
+		 * Save state
+		 */
+		save() {
+			this.editable = false;
+			this.$emit('change', this.vTags);
 		}
 	},
+
+	watch: {
+		/**
+		 * Watch for lazy loading of tags
+		 * 
+		 * @param {Array} tags
+		 */
+		tags(tags) {
+			this.vTags = [].concat(tags);
+		}
+	},
+
+	created() {
+		/* Get all categories and filter 1st level of them */
+		this.list = Object.keys(this.categories.items || []).filter(id => {
+			const item = this.categories.items[id];
+
+			return !item.parent && this.$te(item.name);
+		}).map(id => {
+			const name = this.categories.items[id].name;
+
+			return {
+				id: id,
+				name: this.$t(name),
+				value: name
+			}
+		});
+	}
 }
--- src/components/barter/exchange/list/index.vue
+++ src/components/barter/exchange/list/index.vue
@@ -4,19 +4,57 @@
 
 		<!-- Tags -->
 		<ul class="list">
-			<li v-for="(tag, index) in tags.slice(0, show)" :key="index">{{ tag }}</li>
+			<li
+				v-for="(tag, index) in !editable ? vTags.slice(0, show) : vTags"
+				:key="index"
+			>
+				{{ $t(tag) }}
+				<i
+					v-if="editable"
+					class="fa fa-times remove"
+					@click="remove(index)"
+				></i>
+			</li>
+
+			<li v-show="editable" class="add">
+				<datalist id="categories" ref="list">
+					<option
+						v-for="(item, index) in list"
+						:key="index"
+						:disabled="vTags.includes(item.value)"
+						:data-value="item.value"
+					>{{ item.name }}</option>
+				</datalist>
+
+				<input
+					ref="tag"
+					type="text"
+					list="categories"
+					:placeholder="$t('exchange.add')"
+					@input="validate"
+					@keyup.esc="cancel"
+					@keyup.enter="add"
+				>
+				<i
+					:class="{
+						'fa fa-plus insert': true,
+						'disabled': btnAddDisabled
+					}"
+					@click="add"
+				></i>
+			</li>
 		</ul>
 
 		<!-- Toggle -->
-		<div class="toggle">
+		<div class="toggle" v-if="!editable && vTags.length > visible">
 			<a href="#" class="link" @click.prevent="toggle">
 				{{ $t(`toggle.${ show < tags.length ? 'show' : 'hide' }_all`) }}
 			</a>
 		</div>
 
 		<!-- Tags edit -->
-		<div class="edit" v-if="$slots.edit">
-			<slot name="edit"></slot>
+		<div class="edit" v-if="$slots.edit || $scopedSlots.edit">
+			<slot name="edit" :instance="instance"></slot>
 		</div>
 	</div>
 </template>
