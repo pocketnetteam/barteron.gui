a94462a25e5c2487dcc9247baed76387a67b7bb7
Michael L <levinez.michael@gmail.com>
2023-09-04 12:54:09 +0500

    Add validation to v-form

--- src/components/v-form/v-form/index.js
+++ src/components/v-form/v-form/index.js
@@ -6,27 +6,75 @@ export default {
 			type: String,
 			default: "*[name]"
 		},
+		classes: {
+			type: Object,
+			default: () => ({
+				parent: ".field",
+				passed: "passed",
+				rejected: "rejected"
+			})
+		},
 		rules: {
-			type: Array,
-			default: () => [
-				/* {
-					"input[name='title']": {
-
-					}
-				} */
-			]
+			type: Object,
+			default: () => ({
+				"input[name]": {
+					empty: true, /* Validate for emptity */
+					regex: false, /* Validate with regex */
+					value: "value" /* Check field prop */
+				}
+			})
 		}
 	},
 
 	data() {
 		return {
-			form: this
+			form: this,
+			valid: [],
+			popup: new Popup()
 		}
 	},
 
 	methods: {
+		/**
+		 * Validate fields with rules
+		 */
 		validate() {
+			const form = this.$refs.form;
+			this.valid = [];
 
+			Object.keys(this.rules).forEach(fields => {
+				const rule = this.rules[fields];
+
+				Array.from(
+					form.querySelectorAll(fields)
+				).forEach(field => {
+					const parent = field.closest(this.classes.parent), valid = {};
+
+					if (parent) {
+						parent.classList.remove(this.classes.passed, this.classes.rejected);
+
+						/* Rules */
+						if (rule.empty) {
+							valid.empty = rule.empty && !!field[rule.value];
+						}
+	
+						if (rule.regex) {
+							valid.regex = rule.regex && !!(new RegExp(rule.regex)).test(field[rule.value]);
+						}
+	
+						/* Add class to parent */
+						if (Object.values(valid).includes(false)) {
+							parent.classList.add(this.classes.rejected);
+							this.valid.push(false);
+						} else {
+							parent.classList.add(this.classes.passed);
+							this.valid.push(true);
+						}
+					}
+				});
+			});
+
+			return !Object.values(this.valid).includes(false);
 		},
 
 		/**
@@ -35,7 +83,30 @@ export default {
 		 * @return {Object}
 		 */
 		serialize() {
-			return Object.fromEntries(new FormData(this.$refs.form).entries())
+			return Object.fromEntries(new FormData(this.$refs.form).entries());
+		},
+	}
+}
+
+class Popup {
+	visible = false;
+	icon = "fa fa-spinner fa-spin";
+	text = "just text";
+
+	show() {
+		this.visible = true;
+		return this;
+	}
+
+	hide() {
+		this.visible = false;
+		return this;
+	}
+
+	update(data) {
+		for (let prop in data) {
+			this[prop] = data[prop];
 		}
-	},
+		return this;
+	}
 }
--- src/components/v-form/v-form/index.vue
+++ src/components/v-form/v-form/index.vue
@@ -1,8 +1,17 @@
 <template>
-	<form ref="form">
+	<form class="v-form" ref="form">
 		<fieldset>
 			<slot name="default" :form="form"></slot>
 		</fieldset>
+
+		<div class="popup" v-show="popup.visible">
+			<transition name="fade" mode="in-out">
+				<div class="content">
+					<i :class="`icon ${ popup.icon }`"></i>
+					<p class="text">{{ popup.text }}</p>
+				</div>
+			</transition>
+		</div>
 	</form>
 </template>
 
