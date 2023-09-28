071268e10f700a71b19c63bd76cb2f3303cee70e
Michael L <levinez.michael@gmail.com>
2023-09-28 17:26:07 +0500

    Add number formats to i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,28 +1,42 @@
 import Vue from "vue";
-
 import VueI18n from "vue-i18n";
 
 Vue.use(VueI18n);
 
-const dateTimeFormats = {
-	'en-US': {
-		short: {
-			year: 'numeric',
-			month: 'numeric',
-			day: 'numeric'
-		},
-		middle: {
-			year: 'numeric',
-			month: 'long',
-			day: 'numeric'
-		},
-		long: {
-			year: 'numeric',
-			month: 'long',
-			day: 'numeric'
+const
+	dateFormats = {
+		"en-US": {
+			short: {
+				year: "numeric",
+				month: "numeric",
+				day: "numeric"
+			},
+			middle: {
+				year: "numeric",
+				month: "long",
+				day: "numeric"
+			},
+			long: {
+				year: "numeric",
+				month: "long",
+				day: "numeric"
+			}
 		}
-	}
-}
+	},
+
+	numberFormats = {
+		"en-US": {
+			currency: {
+				style: "currency", currency: "USD"
+			},
+			currencyNoCents: {
+				style: "currency",
+				currency: "USD",
+				minimumFractionDigits: 0,
+				maximumFractionDigits: 0
+			}
+		}
+	};
 
 function loadLocaleMessages() {
 	const
@@ -49,7 +63,8 @@ function loadLocaleMessages() {
 export default new VueI18n({
 	locale: process.env.VUE_APP_I18N_LOCALE || "en-US",
 	fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en-US",
-	messages: loadLocaleMessages(),
 	missingWarn: false,
-	dateTimeFormats
+	messages: loadLocaleMessages(),
+	dateFormats,
+	numberFormats
 });
