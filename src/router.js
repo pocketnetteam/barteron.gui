de70b240da855e4f89aa81b98be9138bfd0632b9
Michael L <levinez.michael@gmail.com>
2023-10-26 10:04:59 +0500

    Add hash to router

--- src/router.js
+++ src/router.js
@@ -60,6 +60,13 @@ const routes = [
 Vue.use(VueRouter);
 
 export default new VueRouter({
-  routes,
-  mode: "history",
+	mode: "history",
+	scrollBehavior: function(to, from, savedPosition) {
+		if (to.hash) {
+			return {selector: to.hash}
+		} else {
+			return {x: 0, y: 0}
+		}
+	},
+	routes
 });
