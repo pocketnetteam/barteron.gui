a451721c38321ecd9c9decc0ea5eb891fb2bddc5
LEV7n <levinez.michael@gmail.com>
2023-10-27 16:16:58 +0500

    Add feedback's content

--- src/components/barter/feedbacks/index.js
+++ src/components/barter/feedbacks/index.js
@@ -2,6 +2,11 @@ export default {
 	name: "Feedbacks",
 
 	props: {
+		form: {
+			type: Boolean,
+			default: false
+		},
+		
 		items: {
 			type: Array,
 			default: () => []
--- src/components/barter/feedbacks/index.vue
+++ src/components/barter/feedbacks/index.vue
@@ -1,6 +1,34 @@
 <template>
-	<div class="feedbacks">
-		Feedbacks
+	<div :class="{
+		'row block t-sep': form,
+		'feedbacks': true
+	}">
+		<v-form v-if="form">
+			<v-textarea
+				ref="feedback"
+				class="field"
+				name="feedback"
+				length="9000"
+				:placeholder="$t('feedbacks.placeholder')"
+			>
+				<template #after>
+					<div class="buttons">
+						<v-button
+							vType="transparent"
+							vSize="sm"
+							class="submit"
+						>
+							<i class="fa fa-paper-plane"></i>
+						</v-button>
+					</div>
+				</template>
+			</v-textarea>
+		</v-form>
+
+		<ul v-if="items.length">
+			<li></li>
+		</ul>
+		<span v-else>{{ $t('feedbacks.empty') }}</span>
 	</div>
 </template>
 
