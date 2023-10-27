c6859a356fbd10b6f1bb1b6f64ee392999bbf31d
Michael L <levinez.michael@gmail.com>
2023-10-27 07:16:57 +0500

    Add v-dialog component

---
+++ src/components/v-form/v-dialog/index.js
@@ -0,0 +1,134 @@
+export default {
+	name: "Vdialog",
+
+	data() {
+		return {
+			visible: false,
+			icon: false,
+			iconColor: "default",
+			text: "Lorem ipsum dolor sit amet consectetur",
+			textColor: "default",
+			buttons: []
+		}
+	},
+
+	methods: {
+		/**
+		 * View presets
+		 * 
+		 * @param {String} [type]
+		 * 
+		 * @return {Vdialog}
+		 */
+		view(type, props) {
+			this.iconColor = type;
+			this.text = props?.text || props;
+			this.textColor = type;
+
+			switch(type) {
+				case "load": {
+					this.icon = props?.icon || "fa-spinner fa-spin";
+					this.buttons = [];
+					break;
+				}
+
+				case "info": {
+					this.icon = props?.icon || "fa-exclamation-circle";
+					this.buttons = props?.buttons || [
+						{ text: this.$t("buttonLabels.ok"), vType: "blue", vSize: "sm", click: () => this.hide() }
+					];
+					break;
+				}
+
+				case "question": {
+					this.icon = props?.icon || "fa-question-circle";
+					this.buttons = props?.buttons || [
+						{ text: this.$t("buttonLabels.no"), vType: "dodoria", vSize: "sm", click: () => this.hide() },
+						{ text: this.$t("buttonLabels.yes"), vType: "blue", vSize: "sm", click: () => this.hide() }
+					];
+					break;
+				}
+
+				case "success": {
+					this.icon = props?.icon || "fa-check-circle";
+					this.buttons = props?.buttons || [
+						{ text: this.$t("buttonLabels.ok"), vType: "roshi", vSize: "sm", click: () => this.hide() }
+					];
+					break;
+				}
+
+				case "warn": {
+					this.icon = props?.icon || "fa-exclamation-triangle";
+					this.buttons = props?.buttons || [
+						{ text: this.$t("buttonLabels.ok"), vType: "hit", vSize: "sm", click: () => this.hide() }
+					];
+					break;
+				}
+
+				case "error": {
+					this.icon = props?.icon || "fa-times-circle";
+					this.buttons = props?.buttons || [
+						{ text: this.$t("buttonLabels.ok"), vType: "dodoria", vSize: "sm", click: () => this.hide() }
+					];
+					break;
+				}
+			}
+
+			this.show();
+
+			return this;
+		},
+
+		/**
+		 * Reset to default state
+		 * 
+		 * @return {Vdialog}
+		 */
+		reset() {
+			this.icon = false;
+			this.iconColor = "default";
+			this.text = "Lorem ipsum dolor sit amet consectetur";
+			this.textColor = "default";
+			this.buttons = [];
+
+			return this;
+		},
+
+		/**
+		 * Show dialog
+		 * 
+		 * @return {Vdialog}
+		 */
+		show() {
+			this.visible = true;
+
+			return this;
+		},
+
+		/**
+		 * Hide dialog
+		 * 
+		 * @return {Vdialog}
+		 */
+		hide() {
+			this.visible = false;
+
+			return this;
+		},
+
+		/**
+		 * Update dialog properties
+		 * 
+		 * @param {Object} data
+		 * 
+		 * @return {Vdialog}
+		 */
+		update(data) {
+			for (let prop in data) {
+				this[prop] = data[prop];
+			}
+
+			return this;
+		}
+	}
+}
---
+++ src/components/v-form/v-dialog/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="v-dialog" v-show="visible">
+		<transition name="fade" mode="in-out">
+			<section class="content">
+				<main>
+					<i :class="`icon color-${ iconColor } fa ${ icon }`"></i>
+					<p :class="`text color-${ textColor }`">{{ text }}</p>
+				</main>
+				<footer v-if="buttons?.length">
+					<v-button
+						v-for="(button, index) in buttons"
+						:key="index"
+						:vType="button.vType"
+						:vSize="button.vSize"
+						@click="button.click"
+					>{{ button.text }}</v-button>
+				</footer>
+			</section>
+		</transition>
+	</div>
+</template>
+
+<style lang="sass" src="./index.sass"></style>
+<script src="./index.js"></script>
