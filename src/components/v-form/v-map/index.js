4c98e395a7b79b2ec4a45301fb2df9ffae667612
LEV7n <levinez.michael@gmail.com>
2023-12-14 11:08:01 +0500

    Make address search field always visible

--- src/components/v-form/v-map/index.js
+++ src/components/v-form/v-map/index.js
@@ -79,7 +79,10 @@ export default {
 		return {
 			mapObject: {},
 			geosearchOptions: {
-				provider: this.provider
+				provider: this.provider,
+				style: "bar",
+				autoClose: true,
+				searchLabel: this.$t("location.enter_address")
 			},
 			marker: this.point
 		}
