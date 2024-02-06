06c8cd3936bdf4a6857ecea3e8c5fd8bd4d2805f
LEV7n <levinez.michael@gmail.com>
2024-02-06 11:56:18 +0500

    Upgrade language switcher

--- src/components/language-switcher/index.js
+++ src/components/language-switcher/index.js
@@ -1,9 +1,50 @@
+import { locales } from "@/i18n/index.js";
+
 export default {
 	name: "LanguageSwitcher",
 
-	methods: {
-		selectLanguage(item) {
-			this.$root.$i18n.locale = item.value;
+	props: {
+		locales: {
+			type: Array,
+			default: () => locales
 		}
+	},
+
+	computed: {
+		/**
+		 * Build locales list
+		 * 
+		 * @returns {Array}
+		 */
+		localesList() {
+			return this.locales.map(l => ({
+				text: l.substring(0, 2).toUpperCase(),
+				value: l,
+				default: l === this.$root.$i18n.locale
+			}));
+		}
+	},
+
+	methods: {
+		/**
+		 * Set locale
+		 * 
+		 * @param {String|Object} item
+		 */
+		selectLanguage(item) {
+			const selected = (() => {
+				if (item?.value) {
+					return item;
+				} else {
+					return this.localesList.filter(f => f.value.includes(item)).pop();
+				}
+			})();
+
+			this.$root.$i18n.locale = selected.value;
+		}
+	},
+
+	created() {
+		/* this.selectLanguage("ru"); */
 	}
 }
