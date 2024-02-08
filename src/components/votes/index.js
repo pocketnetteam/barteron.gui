be55a2e1b90d08fe3ebe9ff0ab6936d2229129b1
LEV7n <levinez.michael@gmail.com>
2024-02-08 21:51:59 +0500

    Add comments component and stuff of votes module

--- src/components/votes/index.js
+++ src/components/votes/index.js
@@ -1,10 +1,12 @@
 import Score from "@/components/score/index.vue";
+import Comment from "@/components/votes/comment.vue";
 
 export default {
 	name: "Votes",
 
 	components: {
-		Score
+		Score,
+		Comment
 	},
 
 	props: {
@@ -19,6 +21,34 @@ export default {
 		items: {
 			type: Array,
 			default: () => []
+		},
+		offerId: String
+	},
+
+	data() {
+		return {
+			loading: false
+		}
+	},
+
+	methods: {
+		submit() {
+			const
+				form = this.$refs.form,
+				feed = this.$refs.feedback,
+				data = form.serialize();
+
+			if (form.validate()) {
+				this.loading = true;
+
+				new this.sdk.models.Comment({
+					postid: this.offerId,
+					message: data.feedback
+				}).set().then(() => {
+					this.loading = false;
+					feed.content = "";
+				});
+			}
 		}
 	}
 }
