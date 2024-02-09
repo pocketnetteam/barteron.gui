c6a2bad389a8c7ed7198882a995e426bb2c70dfd
LEV7n <levinez.michael@gmail.com>
2024-02-09 14:33:51 +0500

    Fix SSR preview

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<?php
-				require_once('og.php'); 
+				require_once("og.php"); 
 
 				$og = new OG($_GET, "http://test.pocketnet.app:8898/");
 
--- public/rpc.php
+++ public/rpc.php
@@ -2,85 +2,88 @@
 
 class RPC {
 
-    protected $node = 'https://pocketnet.app:8899/rpc/';
+	protected $node = 'https://pocketnet.app:8899/rpc/';
 
 	public function __construct ($proxypath)
 	{
 		if (isset($proxypath)){
-            $this->node = $proxypath;
-        }
+			$this->node = $proxypath;
+		}
 	}
 	public function __destruct ()
 	{
 
-    }
-    
-    private function prepareRequest($procedure, array $params = array())
-    {
-        $payload = array(
-            'method' => $procedure,
-        );
-    
-        if (!empty($params)) {
-            $payload['parameters'] = $params;
-        }
-    
-        return $payload;
-    }
+	}
+	
+	private function prepareRequest($procedure, array $params = array())
+	{
+		$payload = array(
+			'method' => $procedure,
+		);
+	
+		if (!empty($params)) {
+			$payload['parameters'] = $params;
+		}
+	
+		return $payload;
+	}
 
-    private function curl($url, $fields){
+	private function curl($url, $fields){
 
 
-        $ch = curl_init();
+		$ch = curl_init();
  
-        curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_POST, count($fields));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-type: application/json", "x-no-compression: 1"));
-        curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($fields));
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        
-        $result = curl_exec($ch);
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch,CURLOPT_POST, count($fields));
+		curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-type: application/json", "x-no-compression: 1"));
+		curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($fields));
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		
+		$result = curl_exec($ch);
 
-        //echo curl_error($ch);
-        curl_close($ch);
+		/* $info = curl_error($ch);
+		var_dump($info); */
 
-        
+		//echo curl_error($ch);
+		curl_close($ch);
 
-        if ($result != false){
-            $json = JSON_decode($result);
+		
 
-            if (isset($json->result) && $json->result != null){
-                $result = $json->data;
-            } else if (isset($json->error)) {
-                $result = $json->error;
-            } else {
-                $result = false;
-            }
-        }
+		if ($result != false){
+			$json = JSON_decode($result);
 
-        return $result;
-    }
+			if (isset($json->result) && $json->result != null){
+				$result = $json->data;
+			} else if (isset($json->error)) {
+				$result = $json->error;
+			} else {
+				$result = false;
+			}
+		}
+
+		return $result;
+	}
 
 	public function send($action, $params){
-        $fields = $this->prepareRequest($action, $params);
-        
-        $url = $this->node."rpc/".$action;
+		$fields = $this->prepareRequest($action, $params);
+		
+		$url = $this->node."rpc/".$action;
 
-        return $this->curl($url, $fields);
-    }
+		return $this->curl($url, $fields);
+	}
 	
-    public function brtoffersbyhashes($hashes){
-        $action = 'getbarteronoffersbyroottxhashes';
-        $params = array($hashes);
+	public function brtoffersbyhashes($hashes){
+		$action = 'getbarteronoffersbyroottxhashes';
+		$params = array($hashes);
 
-        return $this->send($action, $params);
-    }
+		return $this->send($action, $params);
+	}
 
-    public function getuserprofile($address){
-        $action = 'getuserprofile';
-        $params = array(array($address));
+	public function getuserprofile($address){
+		$action = 'getuserprofile';
+		$params = array(array($address));
 
-        return $this->send($action, $params);
-    }
+		return $this->send($action, $params);
+	}
 }
