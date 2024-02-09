ee78b860101069cc007ddb1ba2a34379cd1b4126
LEV7n <levinez.michael@gmail.com>
2024-02-09 20:54:21 +0500

    Map fix

--- src/components/location-switcher/index.js
+++ src/components/location-switcher/index.js
@@ -6,6 +6,7 @@ export default {
 	data() {
 		return {
 			lightbox: false,
+			mapMarker: null,
 			addr: {
 				fetching: false
 			}
@@ -45,12 +46,12 @@ export default {
 		 * 
 		 * @returns {Array|null}
 		 */
-		location: {
-			cache: false,
-			get() {
-				const location = this.sdk.location;
-				return location.latitude ? Object.values(location) : null; 
-			}
+		location() {
+			const location = this.mapMarker || (!this.sdk.empty(this.sdk.location) ? Object.values(this.sdk.location) : null);
+
+			console.log(this.mapMarker, this.sdk.location, location)
+			
+			return Array.isArray(location) && location.length ? location : null;
 		},
 
 		/**
@@ -114,6 +115,16 @@ export default {
 		 */
 		hideLightbox() {
 			this.lightbox = false;
+		},
+
+		/**
+		 * Informing of last marker
+		 * 
+		 * @param {Array} latlng
+		 */
+		setMarker(latlng) {
+			this.mapMarker = latlng;
+			this.addr = {};
 		},
 
 		/**
