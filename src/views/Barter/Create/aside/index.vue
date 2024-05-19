4b0d8dc79d154c042ef84ee03f08ec0b2971342e
Michael L <levinez.michael@gmail.com>
2024-05-20 04:54:43 +0500

    Update steps aside structure

--- src/views/Barter/Create/aside/index.vue
+++ src/views/Barter/Create/aside/index.vue
@@ -4,9 +4,14 @@
 			<menuitem
 				v-for="(step, key) in steps"
 				:key="key"
+				:class="getClass(step)"
 			>
-				<i :class="`fa fa-circle`"></i>
-				<a :href="`#${ key }`">{{ step.text }}</a>
+				<i :class="'fa fa-circle'" v-if="!step.hasOwnProperty('valid')"></i>
+				<i :class="`fa fa-${ step.valid ? 'check' : 'times' }`" v-else></i>
+
+				<a @click="scrollTo(`#${ step.value }`)">
+					{{ $t(`stepsLabels.${ step.value }`) }}
+				</a>
 			</menuitem>
 		</menu>
 	</v-aside>
