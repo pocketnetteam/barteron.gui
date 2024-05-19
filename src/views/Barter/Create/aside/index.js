443db4e32e1cbb5fbeaae5c60fb273874d91d326
Michael L <levinez.michael@gmail.com>
2024-05-20 04:54:18 +0500

    Add steps control for aside

--- src/views/Barter/Create/aside/index.js
+++ src/views/Barter/Create/aside/index.js
@@ -1,14 +1,51 @@
 export default {
 	name: "Aside",
 
-	computed: {
+	data() {
+		return {
+			steps: this.parseLabels("stepsLabels")
+		}
+	},
+
+	methods: {
 		/**
-		 * Get list of sections
+		 * Get class for menuitem
 		 * 
-		 * @returns {Object}
+		 * @param {Boolean} step
 		 */
-		steps() {
-			return this.parseLabels("stepsLabels");
+		getClass(step) {
+			if (step.hasOwnProperty("valid")) {
+				return step.valid ? "passed" : "rejected";
+			}
+
+			return "";
+		},
+
+		/**
+		 * Scroll content viewport to element
+		 * 
+		 * @param {String} selector
+		 */
+		scrollTo(selector) {
+			this.$components.content.scrollTo(selector);
+		},
+
+		/**
+		 * Set state for step
+		 * 
+		 * @param {String} id
+		 * @param {Object} [flag]
+		 * @param {Boolean} [flag.default]
+		 * @param {Boolean} [flag.valid]
+		 */
+		setStep(id, flag) {
+			const index = this.steps.findIndex(step => {
+				return step.value === id;
+			});
+
+			if (index > -1) {
+				this.$set(this.steps, index, { ...this.steps[index], ...flag });
+			}
 		}
 	}
 }
