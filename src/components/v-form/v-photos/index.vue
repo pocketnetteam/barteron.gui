b1a809620ab0649f5fc641f4b7eeb6fe1ec00049
Voyager-3rd <quarts_wingman03@icloud.com>
2024-09-11 01:56:33 +0300

    task103: fix drag-and-drop for images on the offer form

--- src/components/v-form/v-photos/index.vue
+++ src/components/v-form/v-photos/index.vue
@@ -48,9 +48,9 @@
 			class="log"
 		>
 			<li
-				v-for="(item, i) in log"
-				:key="i"
-				:class="`log item-${ item.type }`"
+				v-for="item in log"
+				:key="item.timestamp"
+				:class="`log item-${ item.type } list-${ files.length ? 'filled' : 'empty'}`"
 			>{{ item.text }}</li>
 		</transition-group>
 
