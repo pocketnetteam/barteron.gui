56faf5418ddc7fc63f0fd889958c35a90b5ffd4d
Voyager-3rd <quarts_wingman03@icloud.com>
2024-11-20 17:30:54 +0300

    task171: fix sending message

--- src/views/Barter/ThreeSidedSearch/content/index.vue
+++ src/views/Barter/ThreeSidedSearch/content/index.vue
@@ -17,7 +17,11 @@
 					</ul>
 				</div>
 				<div class="col">
-					<v-button @click="proposeExchange(deal)">{{ $t('buttonLabels.offer_an_exchange') }}</v-button>
+					<v-button 
+						:disabled="isLoading"
+						@click="proposeExchange(deal)"
+					>{{ $t('buttonLabels.offer_an_exchange') }}
+					</v-button>
 				</div>
 			</li>
 		</ul>
