958d59eca5c80a23a3a4e7d41663de81e10473d9
Voyager-3rd <167764214+Voyager-3rd@users.noreply.github.com>
2025-06-17 09:41:01 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,10 @@ module.exports = defineConfig({
 					args[0].template = "./public/index.php";
 					return args;
 				});
+
+			config.output
+				.filename('files/[chunkhash].html')
+				.chunkFilename('files/[chunkhash].html');
 		}
 	},
 
