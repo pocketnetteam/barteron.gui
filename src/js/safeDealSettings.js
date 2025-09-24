ad84baebada3e44aa17845fea0cf38deddcddead
Voyager-3rd <quarts_wingman03@icloud.com>
2025-09-24 13:47:06 +0300

    task538: add safe deal

--- src/js/safeDealSettings.js
+++ src/js/safeDealSettings.js
@@ -5,7 +5,6 @@ const safeDealData = {
 			"THC9eiAxFEA9TFv8jLCsoDuLNsa2gCmw6V",
 			"TJmXy5Q4jgR6QzB7qmncFoURmVMBB95aL6",
 			"TWEE5wJDL2EjR6iryH5mTyebDZWRbPZ733",
-			"TCBZP1MiPCCyevfa31wKp3xQedwVa7Tdn4",
 		],
 
 		production: [
